72159f8568981f969fc2305487b2fb025af45c58
Erick Vivas <88099754+erickvivas415@users.noreply.github.com>
2022-06-24 06:43:53 -0700

    routers

--- movieRater/api/urls.py
+++ movieRater/api/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('api/', include('api.urls')),
-    path('admin/', admin.site.urls),
+    path('', include(router.urls))
+    #path('api/', include('api.urls')),
+    #path('admin/', admin.site.urls),
 ]
